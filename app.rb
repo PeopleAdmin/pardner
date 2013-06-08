47272d9cb1e61343dde75a7b8c263523e24ea272
Joshua Flanagan <joshuaflanagan@gmail.com>
2013-06-08 10:12:01 -0500

    Gather sha, parents, and subject info for each commit

--- app.rb
+++ app.rb
@@ -8,15 +8,42 @@ get '/' do
 end
 
 get '/pending/:from/:to' do
-  commits params[:from], params[:to]
+  commits = pending params[:from], params[:to]
+  commits.join "<br>"
 end
 
 
 private
 
-def commits from, to
-  command = "git log --oneline --first-parent origin/#{from}..origin/#{to}"
-  run_shell command
+class Commit
+  attr_accessor :sha, :subject, :parents
+
+  def self.parse section
+    lines = section.split "\n"
+    info = lines.each_with_object({}) do |line, store|
+      parts = line.split '=', 2
+      store[parts[0]] = parts[1]
+    end
+    new.tap do |commit|
+      commit.sha = info["SHA"]
+      commit.subject = info["SUBJECT"]
+      commit.parents = info["PARENTS"].split(' ') if info["PARENTS"]
+    end
+  end
+
+  def parents
+    @parents ||= []
+  end
+
+  def to_s
+    sha
+  end
+end
+
+def pending from, to
+  command = "git log --format='SHA=%H%nPARENTS=%P%nSUBJECT=%s%n@END@' --first-parent origin/#{from}..origin/#{to}"
+  output = run_shell command
+  output.split("@END@").map {|section| Commit.parse(section.strip.chomp)}
 end
 
 def run_shell command
@@ -27,7 +54,7 @@ def run_shell command
     err << stderr.read.strip
   end
   if result.exitstatus == 0
-    out.gsub(/\n/, "<br>")
+    out
   else
     raise err
   end
