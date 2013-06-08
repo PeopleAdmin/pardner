6baa116515ba8d1f0db0439769c60a4721610e99
Joshua Flanagan <joshuaflanagan@gmail.com>
2013-06-08 08:21:02 -0500

    List commits

--- Gemfile
+++ Gemfile
@@ -1,2 +1,3 @@
 source 'https://rubygems.org'
 gem 'sinatra', '~> 1.3.6'
+gem 'open4', '~> 1.3.0'
--- app.rb
+++ app.rb
@@ -1,7 +1,25 @@
 require 'rubygems'
-require 'bundler/setup'
-require 'sinatra'
+require 'bundler'
+Bundler.require
 
 get '/' do
-  "On Deck"
+  command = "git log --oneline --first-parent origin/production..origin/master"
+  run_shell command
+end
+
+
+private
+
+def run_shell command
+  out = ""
+  err = ""
+  result = Open4::popen4(command) do |pid, stdin, stdout, stderr|
+    out << stdout.read.strip
+    err << stderr.read.strip
+  end
+  if result.exitstatus == 0
+    out.gsub(/\n/, "<br>")
+  else
+    raise err
+  end
 end
